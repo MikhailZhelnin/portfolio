cf98da35014a1f2471114a1c75cfc7ebd7f95727
Mykhailo Zhelnin <mz+digitum@digitum.agency>
2025-08-02 17:48:53 +0300

    start menu fix

--- src/components/Taskbar/Start/StartMenu/StartMenu.tsx
+++ src/components/Taskbar/Start/StartMenu/StartMenu.tsx
@@ -3,10 +3,9 @@ import { forwardRef } from 'react'
 import './styles.scss'
 
 interface IStartMenuProps {
-    someOtherProp?: string;
 }
 
-const StartMenu = forwardRef<HTMLDivElement, IStartMenuProps>(({ someOtherProp }, ref) => {
+const StartMenu = forwardRef<HTMLDivElement, IStartMenuProps>(({ }, ref) => {
     return (
         <div ref={ref} className='startMenu'>StartMenu</div>
     )
